5020302890e78a82ccca72c6d65da39bf1f87aad
Devni  Kumarasinghe <122421704+Devni2000@users.noreply.github.com>
2024-09-22 05:58:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+
+const swiper = new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween:30,
+  
+   //pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    //responsive breakpoints
+    breakpoints:{
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },
+    }
+    
+  });
